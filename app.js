4138b6707dc52353e9e04c4b560a27a3c6b889ea
KuntamukkalaAnvesh <112181431+KuntamukkalaAnvesh@users.noreply.github.com>
2022-10-26 21:45:07 -0500

    added line

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydata = require('./routes/mydata');
-var mydata = require('./routes/computation');
+var computation = require('./routes/computation');
 
 var app = express();
 
